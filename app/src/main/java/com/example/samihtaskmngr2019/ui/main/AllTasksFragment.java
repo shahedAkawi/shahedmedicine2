7a2ec76f329db3b3630429773d8eb3f8df31a0b3
Owner <samih1079@gmail.com>
2019-10-24 14:07:11 +0300

    fixed all getter amd setters add to string

---
+++ app/src/main/java/com/example/samihtaskmngr2019/ui/main/AllTasksFragment.java
@@ -0,0 +1,47 @@
+package com.example.samihtaskmngr2019.ui.main;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.samihtaskmngr2019.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AllTasksFragment extends Fragment {
+
+
+    public AllTasksFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_tasks, container, false);
+    }
+
+
+    public void readTasksFromFirebase()
+    {
+        FirebaseDatabase database=FirebaseDatabase.getInstance();//to connect to database
+        FirebaseAuth auth=FirebaseAuth.getInstance();//to get current UID
+        String uid = auth.getUid();
+        DatabaseReference reference = database.getReference();
+
+
+
+    }
+
+}
