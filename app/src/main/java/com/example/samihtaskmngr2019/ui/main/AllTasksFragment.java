674385e4605d41fe1814e6950c5f35726fd1695c
Owner <samih1079@gmail.com>
2019-10-27 12:22:43 +0200

    fixed all getter amd setters add to string

--- app/src/main/java/com/example/samihtaskmngr2019/ui/main/AllTasksFragment.java
+++ app/src/main/java/com/example/samihtaskmngr2019/ui/main/AllTasksFragment.java
@@ -3,16 +3,22 @@ package com.example.samihtaskmngr2019.ui.main;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.samihtaskmngr2019.R;
+import com.example.samihtaskmngr2019.data.MyTask;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -40,7 +46,22 @@ public class AllTasksFragment extends Fragment {
         String uid = auth.getUid();
         DatabaseReference reference = database.getReference();
 
+        reference.child("tasks").child(uid).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                for (DataSnapshot d : dataSnapshot.getChildren())
+                {
+                    MyTask t=d.getValue(MyTask.class);
+                    Log.d("MYTASK",t.toString());
+                }
+            }
 
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
